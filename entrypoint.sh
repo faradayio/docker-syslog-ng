83a62765857d2546320f790cd31affc74c4335d7
Ivan Beauté <ivan@fabrique-it.fr>
2017-12-11 19:02:39 +0100

    Change the shebang from bash to sh (alpine) Add a custom tcp port for the server configuration file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
-set -e
+#!/bin/sh
 
-if [ -n "$SERVER" ]; then
+if [ -n "$SERVER" ] && [ -n "$TCP_PORT" ]; then
   cp /etc/syslog-ng/syslog-ng-server.conf /etc/syslog-ng/syslog-ng.conf
+  sed -i "s/%TCP_PORT%/$TCP_PORT/g" /etc/syslog-ng/syslog-ng.conf
 elif [ -n "$CLIENT" ] && [ -n "$SYSLOGNG_SERVER" ]  && [ -n "$TCP_PORT" ]; then
   cp /etc/syslog-ng/syslog-ng-client.conf /etc/syslog-ng/syslog-ng.conf
   sed -i "s/%SYSLOGNG_SERVER%/$SYSLOGNG_SERVER/g" /etc/syslog-ng/syslog-ng.conf
